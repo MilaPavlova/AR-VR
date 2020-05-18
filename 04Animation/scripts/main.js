be45183d903151f436ace3fc4a04dcdabb49f7c3
MilaPavlova <65218007+MilaPavlova@users.noreply.github.com>
2020-05-18 10:17:17 +0200

    uf

--- 04Animation/scripts/main.js
+++ 04Animation/scripts/main.js
@@ -1,6 +1,20 @@
 
 
 // // changing individual properties with code and using setInterval
+var rotationSpeed1 = 0.02;
+var myBox = document.getElementById('myBox');
+
+function spin(){
+ myBox.object3D.rotation.x += rotationSpeed1;
+ console.log(myBox.object3D.rotation.x);
+ }
+
+setInterval(spin, 26);
+
+
+
+
+
 var rotationSpeed = 0.01;
 var myOtherBox = document.getElementById('myOtherBox');
 
